9f36bada174dc9b12445248dadb7a83d664b5b79
Mexus <gilaldpellaeon@gmail.com>
2014-11-30 20:32:17 +0300

    Print list every 5 seconds
    Updated todo

--- todo.md
+++ todo.md
@@ -1,10 +1,6 @@
-Library:
-- [x] List interfaces
-- [x] Add a callback for a InterfaceManager::GetList
-
-
 Watcher application:
-- [x] Add/del messages
-- [x] List interfaces
 - [ ] Tests (maybe via python script)
 
+
+Library:
+- [ ] Try to simplify netlink kernel exchange
--- watcher/watcher.cpp
+++ watcher/watcher.cpp
@@ -1,6 +1,7 @@
 #include <libifman/interface_manager.h>
 #include <csignal>
 #include <iostream>
+#include <mutex>
 
 std::atomic_bool running(true);
 
@@ -14,18 +15,24 @@ int main(){
 	std::signal(SIGTERM, SignalHandler);
 	InterfaceManager manager;
 	try {
+		std::mutex coutMutex;
 	        manager.Watch(running, {
-				{RTM_DELLINK, [](const Interface& interface){
-					std::cout << "Removed interface " << interface.name << " (" << interface.Type() << ", " << interface.address << ")\n";
+				{RTM_DELLINK, [&coutMutex](const Interface& interface){
+					std::lock_guard<std::mutex> lg(coutMutex);
+					std::cout << "GONE " << interface.name << " " << interface.address << " " << interface.Type() << "\n";
 				}},
-				{RTM_NEWLINK, [](const Interface& interface){
-					std::cout << "Added interface " << interface.name << " (" << interface.Type() << ", " << interface.address << ")\n";
+				{RTM_NEWLINK, [&coutMutex](const Interface& interface){
+					std::lock_guard<std::mutex> lg(coutMutex);
+					std::cout << "NEW " << interface.name << " " << interface.address << " " << interface.Type() << "\n";
 				}}
 			});
-		std::this_thread::sleep_for(std::chrono::seconds(10));
-	        manager.GetList(running, [](const Interface& interface){
-				std::cout << "Interface " << interface.name << " (" << interface.Type() << ", " << interface.address << ")\n";
-			});
+		while (running){
+		        manager.GetList(running, [&coutMutex](const Interface& interface){
+					std::lock_guard<std::mutex> lg(coutMutex);
+					std::cout << "IFACE " << interface.name << " " << interface.address << " " << interface.Type() << "\n";
+				});
+			std::this_thread::sleep_for(std::chrono::seconds(5));
+		}
 		return 0;
 	} catch (const std::exception& e){
 		std::cerr << "Caught an exception: " << e.what() << "\n";
